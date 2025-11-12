7e2511935abdec24615de973bb9fb03df2f8a715
Samuel Padron <spadron05@gmail.com>
2025-11-12 19:24:29 +0100

    Remove top level main script and separate into two tasks (1) scrape (2) cluster

---
+++ src/clustering/main_cluster.py
@@ -0,0 +1,8 @@
+from preprocessing import load_data, make_embeddings
+from cluster import cluster, visualize
+
+if __name__ == "__main__":
+    lyrics_df = load_data()
+    lyric_embeddings = make_embeddings(lyrics_df)
+    labels = cluster(lyric_embeddings)
+    visualize(lyric_embeddings, labels, lyrics_df)
