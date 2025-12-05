00428cfecdcb3ff514fed5a76aa1e16086116e4d
Samuel Padron <spadron05@gmail.com>
2025-12-05 03:24:04 +0100

    Increase number of songs samples to 100 (5 artists * 20 songs)

--- src/scraper/scrape_artists.py
+++ src/scraper/scrape_artists.py
@@ -18,7 +18,7 @@ def get_artist_song_ids(artist_ids):
         html = safe_request(url)
         
         soup = BeautifulSoup(html, "html.parser")
-        links = soup.find_all(href=re.compile(r'\/global\/en\/lyric\/\d*\/'), limit=10)
+        links = soup.find_all(href=re.compile(r'\/global\/en\/lyric\/\d*\/'), limit=20)
         for a in links:
             href = a.attrs['href']
             song_id = re.sub(r'/global\/en\/lyric\/', '', href)[:-1] # Grab only the id from the href and skip the last '/'
